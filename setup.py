b0480c19f8b49230c69d00d44b8448c75cdae31a
Thibault Gagnaux <tgagnaux@gmail.com>
2023-01-30 17:16:43 +0100

    chore: make openapi2jsonschema executable

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from distutils.core import setup
 setup(name='Openapi2jsonschema',
       version='1.0',
       description='OpenAPI to JSON Schemas converter',
-      author='Yann Hamon',
-      author_email='yann@mandragor.org',
+      author='Thibault Gagnaux',
+      author_email='tgagnaux@gmail.com',
       url='http://github.com/yannh/openapi2jsonschema',
       packages=['openapi2jsonschema'],
+      scripts=[ 'openapi2jsonschema/command.py' ]
       )
